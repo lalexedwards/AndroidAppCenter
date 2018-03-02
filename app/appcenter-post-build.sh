bdc75649b3ee4dfa187fc0efdffa6ffb3df77e45
surrational <36718263+surrational@users.noreply.github.com>
2018-03-02 15:02:53 -0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,10 +1,5 @@
 #find / -iname '*apksigner*'
 
-
-/Users/vsts/agent/2.129.1/scripts/install-keystore.sh
-
-
-
 /Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/.certs/keystore.jks \
 --ks-pass pass:$keystorePass \
 --ks-key-alias $alias \
