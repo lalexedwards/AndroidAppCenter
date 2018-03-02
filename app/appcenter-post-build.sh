f7f4a846b9f25842a8f3a0e8204fa3e4e8592032
surrational <36718263+surrational@users.noreply.github.com>
2018-03-02 13:24:06 -0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,1 +1,4 @@
-find / -iname '*apksigner*'
+#find / -iname '*apksigner*'
+
+/Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner sign -ks $APPCENTER_SOURCE_DIRECTORY/.certs/keystore.jks \
+$APPCENTER_OUTPUT_DIRECTORY/*.apk
