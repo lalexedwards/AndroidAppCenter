3e065fe7b34bcf31488b513325b3929bda0bf9d4
surrational <36718263+surrational@users.noreply.github.com>
2018-03-02 16:17:27 -0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,4 @@
+set -ex
 echo "Signing APK with apksigner"
 /Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/.certs/keystore.jks \
 --ks-pass pass:$keystorePass \
