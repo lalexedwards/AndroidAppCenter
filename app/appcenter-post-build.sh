7cddfb8f6f685f28eb3a32df3ddfdb3ca3bc5510
surrational <surrational@hotmail.com>
2018-03-04 10:26:59 -0800

    test

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,7 +1,10 @@
-set -ex
-echo "Signing APK with apksigner"
-/Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner sign --ks shawn.jks \
---ks-pass pass:$keystorePass \
---ks-key-alias $alias \
---key-pass pass:$keyPass \
-$APPCENTER_OUTPUT_DIRECTORY/*.apk
+#set -ex
+#echo "Signing APK with apksigner"
+#/Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner sign --ks shawn.jks \
+#--ks-pass pass:$keystorePass \
+#--ks-key-alias $alias \
+#--key-pass pass:$keyPass \
+#$APPCENTER_OUTPUT_DIRECTORY/*.apk
+
+
+/Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner verify $APPCENTER_OUTPUT_DIRECTORY/*.apk
