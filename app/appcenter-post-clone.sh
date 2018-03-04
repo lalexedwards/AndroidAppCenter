482dec0c2fd84e1e0496499f9371882ca37e56fd
Shawn Dyas <36718263+surrational@users.noreply.github.com>
2018-03-04 10:36:42 -0800

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,1 +1,3 @@
-ls $APPCENTER_SOURCE_DIRECTORY
+echo 'export APPCENTER_KEYSTORE_PASSWORD=$keystorePass' >> ~/.bashrc 
+echo  'export APPCENTER_KEY_ALIAS=$alias' >> ~/.bashrc 
+echo  'export APPCENTER_KEY_PASSWORD=$keyPass' >> ~/.bashrc 
