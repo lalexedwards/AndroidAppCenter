44caab9485bda2fe6d784e4bdc57ba92b6a2d2a8
Shawn Dyas <36718263+surrational@users.noreply.github.com>
2018-03-04 12:17:09 -0800

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 set -ex
 
 
-echo 'org.gradle.project.keystorePass=93289328' >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
-echo  'org.gradle.project.alias=shawn' >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
-echo  'org.gradle.project.keyPass=93289328' >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
+echo 'org.gradle.project.keystorePass=' >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
+echo  'org.gradle.project.alias=' >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
+echo  'org.gradle.project.keyPass=' >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
 cat $APPCENTER_SOURCE_DIRECTORY/gradle.properties
