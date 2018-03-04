e2c23e75f832a218e7fe8b91be01a2ecdcca1db6
Shawn Dyas <36718263+surrational@users.noreply.github.com>
2018-03-04 11:18:54 -0800

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -5,7 +5,7 @@ echo 'export APPCENTER_KEYSTORE_PASSWORD=$keystorePass' >> ~/.gradlerc
 echo  'export APPCENTER_KEY_ALIAS=$alias' >> ~/.gradlerc 
 echo  'export APPCENTER_KEY_PASSWORD=$keyPass' >> ~/.gradlerc 
 
-source ~/.gradlerc
+#source ~/.gradlerc
 
 cat ~/.bashrc
-cat ~/.gradlerc 
+#cat ~/.gradlerc 
