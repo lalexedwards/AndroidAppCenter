a9624f7eb1250b61aba707477efebec431b5a39a
Shawn Dyas <36718263+surrational@users.noreply.github.com>
2018-03-06 11:16:18 -0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -5,5 +5,11 @@
 #--key-pass pass:$keyPass \
 #$APPCENTER_OUTPUT_DIRECTORY/*.apk
 
+ /Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/.certs/keystore.jks \
+         --ks-pass env:$keystorePass \
+         --ks-key-alias $alias \
+         --key-pass env:$keyPass \
+         $APPCENTER_OUTPUT_DIRECTORY/*.apk
+
 
 /Users/vsts/Library/Android/sdk/build-tools/27.0.3/apksigner verify --print-certs $APPCENTER_OUTPUT_DIRECTORY/*.apk
