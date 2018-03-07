221bfb61ced7a1bd7281f86bcce01ee84f289b1b
Shawn Dyas <36718263+surrational@users.noreply.github.com>
2018-03-06 16:24:03 -0800

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -3,4 +3,6 @@
 #echo 'keystore_password='$keystorePass >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
 #echo 'key_alias='$alias >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
 #echo 'key_password='$keyPass >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
-exit 1 && echo $AGENT_JOBSTATUS 
+
+find blah blah blah
+echo $AGENT_JOBSTATUS 
